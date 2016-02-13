7ab0af29838feb120f2b59587939643858b32773
Jason Rudy <jcrudy@gmail.com>
2016-02-12 20:48:05 -0800

    Added quantile regressor with multiple quantiles and constraints for output range and crossing

--- sklearntools.py
+++ sklearntools.py
@@ -8,12 +8,81 @@ from sklearn.base import BaseEstimator, clone, MetaEstimatorMixin, is_classifier
 import numpy as np
 from sklearn.linear_model.base import LinearRegression
 from sklearn.linear_model.logistic import LogisticRegression
-from sklearn.cross_validation import check_cv, cross_val_score, _fit_and_score
+from sklearn.cross_validation import check_cv, _fit_and_score
 from sklearn.metrics.scorer import check_scoring
 from sklearn.externals.joblib.parallel import Parallel, delayed
 
-class QuantileRegressor(BaseEstimator):
-    _estimator_type = 'regressor'
+from cvxpy import Variable, Minimize, Problem
+from cvxpy.settings import OPTIMAL
+
+class QuantileRegressor(object):
+    def __init__(self, q, prevent_crossing=True, lower_bound=None, upper_bound=None):
+        self.q = q
+        self.prevent_crossing = prevent_crossing
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+    
+    def fit(self, X, y, w):
+        m, n = X.shape
+        p = len(self.q)
+#         variables = []
+#         s_vars = []#Variable(m)
+#         t_vars = []#Variable(m)
+#         b_vars = []#Variable(n)
+        constraints = []
+        objective_function = 0.0
+        last_s = None
+        last_t = None
+        b_vars = []
+        for i in range(p):
+            s = Variable(m)
+            t = Variable(m)
+            b = Variable(n)
+            b_vars.append(b)
+            tau = self.q[i]
+            constraints.append(s >= 0)
+            constraints.append(t >= 0)
+            constraints.append(y - X * b == s - t)
+            objective_function += tau * w * s + \
+                                  (1 - tau) * w * t
+            if i >= 1 and self.prevent_crossing:
+                constraints.append(s <= last_s)
+                constraints.append(t >= last_t)
+            if self.lower_bound is not None:
+                constraints.append(X * b >= self.lower_bound)
+            if self.upper_bound is not None:
+                constraints.append(X * b <= self.upper_bound)
+            last_s = s
+            last_t = t
+        objective = Minimize(objective_function)
+        problem = Problem(objective, constraints)
+        problem.solve()
+        if problem.status != OPTIMAL:
+            raise ValueError('Problem status is: %s' % problem.status)
+        self.coef_ = np.array([b.value for b in b_vars])
+        return self
+    
+    def predict(self, X):
+        return np.dot(X, self.coef_)
+
+def sim_quantiles(taus, quantiles):
+    assert quantiles.shape[1] == len(taus)
+    p_vals = np.array([taus[i] - taus[i-1] for i in range(1,len(taus))])
+    choices = np.random.multinomial(1, p_vals, size=quantiles.shape[0])
+    lowers = quantiles[:, :-1][choices==1]
+    uppers = quantiles[:, 1:][choices==1]
+    y = np.random.uniform(low=lowers, high=uppers)
+    return y
+
+def test_quantile_regression():
+    X = np.random.uniform(0,1,size=(10000,2))
+    b = np.array([[0,0],[1,1],[2,2],[4,4],[6,6]]).transpose()
+    quantiles = np.dot(X, b)
+    taus = np.array([0.,.1,.5,.9,1.])
+    y = sim_quantiles(taus, quantiles)
+    w = np.ones_like(y)
+    qr = QuantileRegressor(taus[1:-1]).fit(X, y, w)
+    assert np.all(np.max(np.abs(np.array(qr.b_vals_).transpose() - b[:, 1:-1])) < .2)
 
 def weighted_average_score_combine(scores):
     scores_arr = np.array(scores)
@@ -212,8 +281,9 @@ def test_multiple_response_regressor():
 
 
 if __name__ == '__main__':
-    test_multiple_response_regressor()
+    test_quantile_regression
     test_backward_elimination_estimator_cv()
+    test_multiple_response_regressor()
     print 'Success!'
     
     
