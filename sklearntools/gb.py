223a858dd76eacbf8a57425e2844b6f53793d98a
Jason Rudy <jcrudy@gmail.com>
2017-08-12 14:15:32 -0700

    Fixed some loss function bugs

--- sklearntools/gb.py
+++ sklearntools/gb.py
@@ -4,7 +4,9 @@ from toolz.dicttoolz import valmap
 from .line_search import golden_section_search, zoom_search, zoom
 import numpy as np
 from sklearn.ensemble.gradient_boosting import RegressionLossFunction,\
-    QuantileEstimator
+    QuantileEstimator, QuantileLossFunction
+from scipy.optimize.linesearch import line_search
+from matplotlib import pyplot as plt
 
 def log_one_plus_exp_x(x):
     lower = -10.
@@ -40,18 +42,20 @@ class SmoothQuantileLossFunction(RegressionLossFunction):
         return QuantileEstimator(self.tau)
     
     def __call__(self, y, pred, sample_weight=None):
-        x =  y - pred
+        x = y - pred
+        n = float(y.shape[0])
         if sample_weight is not None:
-            return np.dot(sample_weight, (self.tau * x + self.alpha * log_one_plus_exp_x(-(1./self.alpha)*x)))
+            return (1/n) * np.dot(sample_weight, (self.tau * x + self.alpha * log_one_plus_exp_x(-(1./self.alpha)*x)))
         else:
-            return np.sum(self.tau * x + self.alpha * log_one_plus_exp_x(-(1./self.alpha)*x))
+            return (1/n) * np.sum(self.tau * x + self.alpha * log_one_plus_exp_x(-(1./self.alpha)*x))
     
     def negative_gradient(self, y, pred, sample_weight=None):
         x =  y - pred
+        n = float(y.shape[0])
         if sample_weight is not None:
-            return sample_weight * (one_over_one_plus_exp_x(-(1. / self.alpha) * x) - self.tau)
+            return  sample_weight * (self.tau - one_over_one_plus_exp_x((1. / self.alpha) * x))
         else:
-            return one_over_one_plus_exp_x(-(1. / self.alpha) * x) - self.tau
+            return (self.tau - one_over_one_plus_exp_x((1. / self.alpha) * x))
         
     def _update_terminal_region(self, *args, **kwargs):
         raise NotImplementedError()
@@ -64,7 +68,8 @@ class GradientBoostingEstimator(STSimpleEstimator):
         self.n_estimators = n_estimators
         
     def fit(self, X, y, sample_weight=None, exposure=None):
-        
+        qloss = QuantileLossFunction(1, self.loss_function.tau)
+        qqloss = SmoothQuantileLossFunction(1, self.loss_function.tau, 0.000001)
         initial_estimator = self.loss_function.init_estimator()
         fit_args = self._process_args(X=X, y=y, sample_weight=sample_weight, 
                                       exposure=exposure)
@@ -83,39 +88,71 @@ class GradientBoostingEstimator(STSimpleEstimator):
             gradient_args['exposure': exposure]
         gradient = shrinkd(1, self.loss_function.negative_gradient(**valmap(shrinkd(1), gradient_args)))
         partial_arguments = {'y':y}
+        y_base = y.copy()
         if sample_weight is not None:
             partial_arguments['sample_weight'] = sample_weight
         if exposure is not None:
             partial_arguments['exposure'] = exposure
         loss_function = lambda pred: self.loss_function(pred=shrinkd(1, pred), **valmap(shrinkd(1), partial_arguments))
+#         loss_grad = lambda pred: -self.loss_function.negative_gradient(pred=pred, **valmap(shrinkd(1), partial_arguments))
         self.initial_loss_ = loss_function(prediction)
-        for _ in range(self.n_estimators):
+        loss = self.initial_loss_
+        losses = [self.initial_loss_]
+        for i in range(self.n_estimators):
+            print 'loss =', loss, self.loss_function(np.ravel(y),np.ravel(prediction))
+            print 'qloss =', qloss(np.ravel(y),np.ravel(prediction))
+            print 'qqloss =', qqloss(np.ravel(y),np.ravel(prediction))
+            assert np.all(partial_arguments['y'] == y_base)
             fit_args['y'] = gradient
+            print gradient
             estimator = clone(self.base_estimator)
             approx_gradient = shrinkd(1, fit_predict(estimator, **valmap(shrinkd(1), fit_args)))
             print 'norm(gradient) =', np.linalg.norm(gradient)
             print 'norm(approx_gradient) =', np.linalg.norm(approx_gradient)
-#             loss_grad = lambda pred: -self.loss_function.negative_gradient(pred=pred, **valmap(shrinkd(1), partial_arguments))
+            print 'gradient . approx_gradient =', np.dot(gradient, approx_gradient)
+#             alpha = zoom_search(golden_section_search(1e-12), zoom(1., 20, 2.), loss_function, prediction, approx_gradient)
+#             alpha, _, _, _, _, _ = line_search(loss_function, loss_grad, shrinkd(1, prediction), shrinkd(1,approx_gradient))
             alpha = zoom_search(golden_section_search(1e-12), zoom(1., 20, 2.), loss_function, prediction, approx_gradient)
-#             alpha, _, _, _, _, _ = line_search(loss_function, loss_grad, shrinkd(1, prediction), shrinkd(1,gradient))
+            print 'alpha =', alpha
+#             print 'other_alpha =', other_alpha
+#             x = np.arange(-50, 50, .1)
+#             y = np.empty_like(x)
+#             for i in range(len(x)):
+#                 y[i] = loss_function(prediction + approx_gradient * x[i])
+#             plt.plot(x, y)
+#             plt.show()
+#             z = np.empty_like(x)
+#             for i in range(len(x)):
+#                 z[i] = loss_function(prediction + gradient * x[i])
+#             plt.plot(x, y)
+#             plt.show()
             if alpha is None:
 #                 alpha = self.max_step_size / 10.
+#                 alpha = zoom_search(golden_section_search(1e-12), zoom(1., 20, 2.), loss_function, prediction, approx_gradient)
                 raise ValueError('Line search did not converge')
-            print 'alpha =', alpha
+            
             estimators.append(estimator)
             coefficients.append(alpha)
             delta = alpha * approx_gradient
             prediction += delta
             loss = loss_function(prediction)
+            
+            losses.append(loss)
 #             if np.linalg.norm(delta) / float(len(delta)) < 1e-10:
 #                 pass
+            if abs(losses[-1] - losses[-2]) < 1e-10:
+                print 'early terminate after %d iterations' % (i+1)
+                break
+            print 'loss_diff =', abs(losses[-1] - losses[-2])
             gradient_args['pred'] = prediction
             gradient = shrinkd(1, self.loss_function.negative_gradient(**valmap(shrinkd(1), gradient_args)))
             
-            if np.linalg.norm(gradient) / float(len(gradient)) < 1e-10:
-                break
+#             if np.linalg.norm(gradient) / float(len(gradient)) < 1e-10:
+#                 break
+        print 'all %d iterations completed' % (i+1)
         self.coefficients_ = coefficients
         self.estimators_ = estimators
+        self.losses_ = losses
         self.score_ = (self.initial_loss_ - loss) / self.initial_loss_
     
     def score(self, X, y, sample_weight=None, exposure=None):
--- sklearntools/test/test_gb.py
+++ sklearntools/test/test_gb.py
@@ -4,7 +4,47 @@ from sklearntools.gb import GradientBoostingEstimator,\
 from numpy.testing.utils import assert_approx_equal, assert_array_almost_equal
 from sklearntools.earth import Earth
 from nose.tools import assert_less, assert_greater
+from sklearn.ensemble.gradient_boosting import GradientBoostingRegressor,\
+    QuantileLossFunction
+from sklearn.ensemble.bagging import BaggingRegressor
+from sklearn.metrics.regression import r2_score
+from sklearn.tree.tree import DecisionTreeRegressor
+from nose import SkipTest
+from distutils.version import LooseVersion
+import sklearn
+from types import MethodType
 
+# Patch over bug in scikit learn (issue #9539)
+if LooseVersion(sklearn.__version__) <= LooseVersion('0.18.2'):
+    def __call__(self, y, pred, sample_weight=None):
+        pred = pred.ravel()
+        diff = y - pred
+        alpha = self.alpha
+    
+        mask = y > pred
+        if sample_weight is None:
+            loss = (alpha * diff[mask].sum() -
+                    (1.0 - alpha) * diff[~mask].sum()) / y.shape[0]
+        else:
+            loss = ((alpha * np.sum(sample_weight[mask] * diff[mask]) -
+                    (1.0 - alpha) * np.sum(sample_weight[~mask] * diff[~mask])) /
+                    sample_weight.sum())
+        return loss
+    QuantileLossFunction.__call__ = MethodType(__call__, None, QuantileLossFunction)
+    
+def test_smooth_quantile_loss_function():
+    np.random.seed(0)
+    n = 1000
+    y1 = np.random.normal(size=n)
+    y2 = np.random.normal(size=n)
+    tau = .75
+    alpha = .000001
+    l1 = SmoothQuantileLossFunction(1, tau, alpha)
+    l2 = QuantileLossFunction(1, tau)
+    print 'l1 =', l1(y1, y2)
+    print 'l2 =', l2(y1, y2)
+    assert_approx_equal(l1(y1, y2), l2(y1, y2))
+    
 def test_log_one_plus_exp_x():
     x = np.arange(-20.,100.)
     y_1 = np.log(1+np.exp(x))
@@ -18,19 +58,34 @@ def test_one_over_one_plus_exp_x():
     assert_array_almost_equal(y_1, y_2)
 
 def test_gradient_boosting_estimator():
-    np.random.seed(1)
+    np.random.seed(0)
     m = 10000
     n = 10
-    p = .5
+    p = .8
     X = np.random.normal(size=(m,n))
     beta = np.random.normal(size=n)
     mu = np.dot(X, beta)
     y = np.random.lognormal(mu)
-    model = GradientBoostingEstimator(Earth(verbose=True), SmoothQuantileLossFunction(1, p, .01), n_estimators=100)
+    loss_function = SmoothQuantileLossFunction(1, p, .0001)
+    q_loss = QuantileLossFunction(1, p)
+    model = GradientBoostingEstimator(Earth(), loss_function, n_estimators=100)
     model.fit(X, y)
     prediction = model.predict(X)
+    model2 = GradientBoostingRegressor(loss='quantile', alpha=p)
+    model2.fit(X, y)
+    prediction2 = model2.predict(X)
+    model_loss = loss_function(y, prediction)
+    model2_loss = loss_function(y, prediction2)
+    
+    print 'loss1 =', model_loss
+    print 'loss2 =', model2_loss
+    print 'qloss1 =', q_loss(y, prediction)
+    print 'qloss2 =', q_loss(y, prediction2)
+    print 'r2_score_1 =', r2_score(y,prediction)
+    print 'r2_score_2 =', r2_score(y,prediction2) 
     q = np.mean(y <= prediction)
-    print q
+    q2 = np.mean(y <= prediction2)
+    print q, q2
     print model.score_
     assert_less(np.abs(q-p), .05)
     assert_greater(model.score_, 0.)
