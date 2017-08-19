642b281b60034a249ff3948bb575777d8b5c44c9
Jason Rudy <jcrudy@gmail.com>
2017-08-19 14:14:56 -0700

    Fix incompatibility with sklearn 0.19 around n_features vs n_features_

--- sklearntools/sym/adapters/gradient_boosting.py
+++ sklearntools/sym/adapters/gradient_boosting.py
@@ -1,5 +1,4 @@
 from sympy.core.numbers import RealNumber
-from sympy.functions.elementary.exponential import exp
 from sklearn.ensemble.gradient_boosting import BinomialDeviance,\
     LogOddsEstimator, GradientBoostingClassifier, QuantileEstimator,\
     LossFunction
@@ -7,18 +6,20 @@ from ..base import call_method_or_dispatch
 from operator import add
 from ..sym_predict_proba import register_sym_predict_proba
 from ..sym_predict import sym_predict
-from sklearntools.sym.input_size import register_input_size,\
-    input_size_from_n_features
+from ..input_size import register_input_size,\
+    input_size_from_n_features, input_size_from_n_features_
 from sklearntools.sym.sym_predict import register_sym_predict
 from sympy.core.symbol import Symbol
 from sklearntools.sym.syms import register_syms
 from sympy import exp
 from sklearntools.sym.sym_score_to_proba import register_sym_score_to_proba,\
     sym_score_to_proba
-from sympy.functions.elementary.miscellaneous import Max
 from sklearntools.sym.sym_score_to_decision import register_sym_score_to_decision
 from sympy.functions.elementary.piecewise import Piecewise
 from sympy.logic.boolalg import true
+from ..base import fallback
+from distutils.version import LooseVersion
+import sklearn
 
 def sym_binomial_deviance_score_to_proba(loss, score):
     return RealNumber(1) / (RealNumber(1) + exp(RealNumber(-1) * score))
@@ -41,7 +42,7 @@ def sym_predict_proba_gradient_boosting_classifier(estimator):
     return sym_loss_function(estimator.loss_, tree_part + init_part)
 
 register_sym_predict_proba(GradientBoostingClassifier, sym_predict_proba_gradient_boosting_classifier)
-register_input_size(GradientBoostingClassifier, input_size_from_n_features)
+register_input_size(GradientBoostingClassifier, input_size_from_n_features if LooseVersion(sklearn.__version__) < LooseVersion('0.19') else input_size_from_n_features_)
 
 def sym_predict_quantile_estimator(estimator):
     return RealNumber(estimator.quantile)
