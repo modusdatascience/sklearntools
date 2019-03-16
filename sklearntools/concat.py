6d0367cb4325fe889f85e79c136b85ca3e09350b
Jason Rudy <jcrudy@gmail.com>
2019-03-15 17:23:22 -0700

    ConcatenatingEstimator

---
+++ sklearntools/concat.py
@@ -0,0 +1,66 @@
+from .sklearntools import STSimpleEstimator, growd
+from operator import methodcaller, __add__
+from sklearn.base import clone
+from toolz.functoolz import compose, flip
+import pandas
+import numpy as np
+from collections import OrderedDict
+from six.moves import reduce
+
+def best_type(types, arg, *args):
+    while not isinstance(arg, types[-1]):
+        types.pop()
+        if not types:
+            raise TypeError('No best type found.')
+    if not args:
+        return types[-1]
+    return best_type(types, *args)
+        
+def safe_concat(*args):
+    try:
+        return_type = best_type(
+                                [np.ndarray, pandas.DataFrame],
+                                *args
+                                )
+    except TypeError:
+        raise TypeError('Inputs must be either pandas DataFrames or numpy ndarrays.')
+    if return_type is np.ndarray:
+        return np.concatenate(tuple(map(growd(2), args)), axis=1)
+    else:
+        columns = reduce(__add__, map(compose(list, flip(getattr)('columns')), args))
+        result = pandas.concat(args, axis=1, ignore_index=True)
+        result.columns = columns
+        return result
+        
+    
+class ConcatenatingEstimator(STSimpleEstimator):
+    def __init__(self, estimators):
+        self.estimators = tuple(estimators)
+        self.ordered_estimators = OrderedDict(self.estimators.items() if hasattr(self.estimators, 'items') else self.estimators)
+
+    
+    def fit(self, X, y=None, sample_weight=None, exposure=None):
+        fit_args = self._process_args(X=X, y=y, sample_weight=sample_weight,
+                                      exposure=exposure)
+        self.estimators_ = OrderedDict(
+                                       zip(
+                                           self.ordered_estimators.keys(), 
+                                           map(
+                                               compose(
+                                                       methodcaller('fit', **fit_args), 
+                                                       clone
+                                                       ),
+                                               self.ordered_estimators.values()
+                                               )
+                                           )
+                                       )
+    
+    def predict(self, X, exposure=None):
+        args = self._process_args(X=X, exposure=exposure)
+        return safe_concat(*map(methodcaller('predict', **args), self.estimators_.values()))
+    
+    def transform(self, X, exposure=None):
+        args = self._process_args(X=X, exposure=exposure)
+        return safe_concat(*map(methodcaller('transform', **args), self.estimators_.values()))
+    
+    
---
+++ sklearntools/test/test_concat.py
@@ -0,0 +1,33 @@
+from sklearntools.concat import ConcatenatingEstimator
+from sklearntools.calibration import IdentityTransformer
+from sklearntools.super_learner import OrderTransformer, sort_rows_independently
+import pandas
+import numpy as np
+from nose.tools import assert_list_equal
+from numpy.testing.utils import assert_array_equal
+
+
+def test_concatenating_estimator_transform():
+    m = 100
+    n = 10
+    model = ConcatenatingEstimator([
+                                    ('identity',IdentityTransformer()),
+                                    ('order', OrderTransformer())
+                                    ])
+    data = pandas.DataFrame(np.random.normal(size=(m,n)), columns=['x%d' % i for i in range(n)])
+    model.fit(data)
+    output = model.transform(data)
+    assert_list_equal(list(data.columns) + ['O(%d/%d)' % (i + 1, n) for i in range(n)], list(output.columns))
+    assert_array_equal(output.iloc[:,:n], data)
+    assert_array_equal(output.iloc[:,n:], 
+                       sort_rows_independently(np.asarray(data), inplace=False))
+    
+if __name__ == '__main__':
+    import sys
+    import nose
+    # This code will run the test in this file.'
+    module_name = sys.modules[__name__].__file__
+
+    result = nose.run(argv=[sys.argv[0],
+                            module_name,
+                            '-s', '-v'])
